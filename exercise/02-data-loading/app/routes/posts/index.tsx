915980ff343de67bb49c093b35bf0f492105fc61
Maulana Adil <maulanaadil.latif@gmail.com>
2022-08-04 11:39:50 +0700

    working on exercise-02

--- exercise/02-data-loading/app/routes/posts/index.tsx
+++ exercise/02-data-loading/app/routes/posts/index.tsx
@@ -1,7 +1,33 @@
+import { useLoaderData } from "@remix-run/react";
+import { Link } from "react-router-dom";
+import { json } from "@remix-run/node";
+import { prisma } from "~/db.server";
+
+async function getPosts() {
+  return prisma.post.findMany();
+}
+
+export const loader = async () => {
+  return json({
+    posts: await getPosts(),
+  });
+};
+
 export default function Posts() {
+  const { posts } = useLoaderData<typeof loader>();
+
   return (
     <main>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
